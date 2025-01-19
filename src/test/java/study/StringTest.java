3b7ea45297fae22b9dec0b94f32c61a82b6ec876
Kimhannah <skqkdldhf98@naver.com>
2025-01-19 15:30:47 +0900

    test: 요구사항1 split 테스트 코드 작성

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,5 +10,21 @@ public class StringTest {
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    @DisplayName("\"1,2\".split(\",\")는 [\"1\", \"2\"]를 반환한다.")
+    void test1() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).contains("1");
+        assertThat(actual).contains("2");
+        assertThat(actual).containsExactly("1", "2");
+    }
+
+    @Test
+    @DisplayName("\"1\".split(\",\")는 [\"1\"]을 반환한다.")
+    void test2() {
+        String[] actual = "1".split(",");
+        assertThat(actual).containsExactly("1");
     }
 }
