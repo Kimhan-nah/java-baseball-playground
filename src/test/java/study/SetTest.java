6a868b9300b41b4a165492ad551d3c130648321e
Kimhannah <skqkdldhf98@naver.com>
2025-01-19 16:00:51 +0900

    test: 요구사항2 ParameterizedTest로 테스트 작성

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,8 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -25,5 +27,12 @@ public class SetTest {
     @DisplayName("numbers의 크기는 3이다.")
     void test1() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("numbers에 값이 존재할 경우, contains는 true를 반환한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void test2(int input) {
+        assertThat(numbers.contains(input)).isTrue();
     }
 }
