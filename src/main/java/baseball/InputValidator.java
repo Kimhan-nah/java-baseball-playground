b571f087bcd249b0429a46093f647e113549eb81
Kimhannah <skqkdldhf98@naver.com>
2025-02-02 16:59:16 +0900

    feat: 입력값 유효성 검증하는 InputValidator 생성

---
+++ src/main/java/baseball/InputValidator.java
@@ -0,0 +1,21 @@
+package baseball;
+
+public class InputValidator {
+    public boolean checkInput(String input, int length) {
+        boolean[] used = new boolean[10];
+
+        // 문자열 길이 확인
+        if (input.length() != length) {
+            return false;
+        }
+
+        for (char ch : input.toCharArray()) {
+            if (!Character.isDigit(ch)) return false;
+            used[ch - '0'] = true;
+        }
+
+        // 사용중인 숫자 갯수 확인
+        return Validator.checkLength(used, length);
+    }
+
+}
