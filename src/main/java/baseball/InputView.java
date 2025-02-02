ca7056c7519bfd4b7820267bcea0d03cb0343ee1
Kimhannah <skqkdldhf98@naver.com>
2025-02-02 18:14:04 +0900

    refactor: InputView 입력받는 메서드 반환값 String으로 수정

--- src/main/java/baseball/InputView.java
+++ src/main/java/baseball/InputView.java
@@ -5,10 +5,9 @@ import java.util.Scanner;
 public class InputView {
     private static final String inputNumberPrompt = "숫자를 입력해 주세요 : ";
 
-    public int readNumber() {
+    public String readLine() {
         System.out.print(inputNumberPrompt);
         Scanner scanner = new Scanner(System.in);
-
-        return scanner.nextInt();
+        return scanner.nextLine();
     }
 }
